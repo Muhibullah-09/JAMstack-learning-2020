927cb48584ed0fbe5dbf381432ad2f76c259f720
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-10-16 10:04:11 +0500

    step 14 completed
    deploy react application on netlify

---
+++ 14-react-netlify-helloworld/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World from netlify CLI and React</h1>
+    </div>
+  );
+}
+
+export default App;
